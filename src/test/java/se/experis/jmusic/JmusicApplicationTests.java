5fd2d045eeaaf0732c69df078387aca222eade26
Oscar Dahlquist <Oscar.Dahlquist@gmail.com>
2020-10-15 13:20:28 +0200

    second commit

---
+++ src/test/java/se/experis/jmusic/JmusicApplicationTests.java
@@ -0,0 +1,13 @@
+package se.experis.jmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
