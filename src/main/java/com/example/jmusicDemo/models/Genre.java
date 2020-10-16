da001bc175e683e6807e8e79c2c6921469763380
Oscar Dahlquist <Oscar.Dahlquist@gmail.com>
2020-10-16 18:07:37 +0200

    Vattenkruka - Feat: Created the class for genre, it's setters and getters as well as constructor

---
+++ src/main/java/com/example/jmusicDemo/models/Genre.java
@@ -0,0 +1,28 @@
+package com.example.jmusicDemo.models;
+
+public class Genre {
+    private int genreId;
+    private String name;
+
+    public Genre(int genreId, String name) {
+        this.genreId = genreId;
+        this.name = name;
+    }
+
+    public int getGenreId() {
+        return genreId;
+    }
+
+    public void setGenreId(int genreId) {
+        this.genreId = genreId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
