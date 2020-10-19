d37cee0e91471e26a6274e22e3b745c44649568d
Oscar Dahlquist <Oscar.Dahlquist@gmail.com>
2020-10-19 23:54:30 +0200

    Vattenkruka - Feat: Artistrepo with function to get a random list of artists

---
+++ src/main/java/com/example/jmusicDemo/data_access/ArtistRepository.java
@@ -0,0 +1,37 @@
+package com.example.jmusicDemo.data_access;
+
+import com.example.jmusicDemo.models.Artist;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class ArtistRepository {
+
+    public static ArrayList<Artist> getRandomArtists(){
+
+        var artistList = new ArrayList<Artist>(int amountOfArtists);
+        Connection conn = null;
+        String sql = "SELECT ArtistId, Name FROM Artist ORDER BY RANDOM() LIMIT?";
+
+        try {
+            conn = ConnectionHelper.getConnection();
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setInt(1,amountOfArtists);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while(resultSet.next()){
+                artistList.add(new Artist(resultSet.getInt("ArtistId"), resultSet.getString("Name")));
+            }
+        }
+        catch(SQLException ex){
+            ex.printStackTrace();
+        } finally {
+            ConnectionHelper.close(conn);
+        }
+        return artistList;
+    }
+}
