c260c80df305f4cace55fac79f02a44aeb2e0d21
Per Christer Vågsäter <per-christer.vagsater@se.experis.com>
2020-10-20 21:12:42 +0200

    PerChrister addedd some file handling for api

--- src/main/java/com/example/jmusicDemo/controllers/CustomerController.java
+++ src/main/java/com/example/jmusicDemo/controllers/CustomerController.java
@@ -3,6 +3,8 @@ package com.example.jmusicDemo.controllers;
 import com.example.jmusicDemo.data_access.CustomerRepository;
 import com.example.jmusicDemo.models.Customer;
 import com.example.jmusicDemo.models.PopularGenre;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -25,7 +27,20 @@ public class CustomerController {
 
     @RequestMapping(value = "/api/customers/add", method = RequestMethod.POST)
     public Boolean addCustomer(@RequestBody Customer customer) {
-        return customerRepository.addCustomer(customer);
+
+        if (customer.getFirstName() == null || customer.getLastName() == null || customer.getEmail() == null) {
+            new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+
+        boolean customerIsCreated = customerRepository.addCustomer(customer);
+
+        if (customerIsCreated) {
+            new ResponseEntity(HttpStatus.CREATED);
+        } else {
+            new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return customerIsCreated;
     }
 
     @RequestMapping(value = "/api/customers/country", method = RequestMethod.GET)
@@ -34,9 +49,17 @@ public class CustomerController {
     }
 
     @RequestMapping(value = "/api/customers/update", method = RequestMethod.PUT)
-    public Boolean updateCustomer(@RequestBody Customer customer) {
-        return customerRepository.updateCustomer(customer);
+    public ResponseEntity updateCustomer(@RequestBody Customer customer) {
+        boolean isUpdated = customerRepository.updateCustomer(customer);
+
+        if (isUpdated) {
+            return new ResponseEntity(HttpStatus.OK);
+        } else {
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
+
+
 
     @RequestMapping(value = "/api/customers/invoice/total", method = RequestMethod.GET)
     public LinkedHashMap<String, Double> getHighestSpenders() {
