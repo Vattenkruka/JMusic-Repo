93ee99bcab4092ebb6726a8e88a38587df41351f
Oscar Dahlquist <Oscar.Dahlquist@gmail.com>
2020-10-20 10:12:13 +0200

    Vattenkruka - Feat: getRandomGenres function created for main page

--- src/main/java/com/example/jmusicDemo/controllers/ViewController.java
+++ src/main/java/com/example/jmusicDemo/controllers/ViewController.java
@@ -1,6 +1,7 @@
 package com.example.jmusicDemo.controllers;
 
 import com.example.jmusicDemo.data_access.ArtistRepository;
+import com.example.jmusicDemo.data_access.GenreRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ public class ViewController {
     @GetMapping(value = "/")
     public String home(Model model){
         model.addAttribute("artistlist", ArtistRepository.getRandomArtists(5));
+        model.addAttribute("genreList", GenreRepository.getRandomGenres(5));
         return "home";
     }
 
---
+++ src/main/java/com/example/jmusicDemo/data_access/GenreRepository.java
@@ -0,0 +1,37 @@
+package com.example.jmusicDemo.data_access;
+
+import com.example.jmusicDemo.models.Genre;
+
+import javax.xml.transform.Result;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class GenreRepository {
+    public static ArrayList<Genre> getRandomGenres(int amountOfGenres){
+        var genreList = new ArrayList<Genre>();
+            Connection conn = null;
+            String sql = "SELECT GenreId, Name FROM Genre ORDER BY RANDOM() LIMIT?";
+
+            try{
+                conn = ConnectionHelper.getConnection();
+                PreparedStatement preparedStatement = conn.prepareStatement(sql);
+                preparedStatement.setInt(1,amountOfGenres);
+
+                ResultSet resultSet = preparedStatement.executeQuery();
+
+                while(resultSet.next()){
+                    genreList.add((new Genre(resultSet.getInt("GenreId"),resultSet.getString("Name"))));
+                }
+            }
+            catch(SQLException ex){
+                ex.printStackTrace();
+            }
+            finally {
+                ConnectionHelper.close(conn);
+            }
+            return genreList;
+    }
+}
